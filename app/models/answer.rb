33e82f216558ee56439b3e14a5e24c7754f48c1b
julian <julian@juliankulesh.com>
2015-06-03 12:04:37 +0300

    add question_id presence validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
 
+  validates :question_id, presence: true
   validates :body, presence: true
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
+  it { should validate_presence_of :question_id }
   it { should belong_to :question }
 end
