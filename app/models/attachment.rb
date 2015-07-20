c9033149b9c7a23b0282f8665ad82af7def6d7a3
raventid <juliankul@gmail.com>
2015-07-20 13:30:43 +0300

    adds nested_attributtes for question

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,2 +1,5 @@
 class Attachment < ActiveRecord::Base
+  belongs_to :question
+
+  mount_uploader :file, FileUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720093320) do
+ActiveRecord::Schema.define(version: 20150720094633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,12 @@ ActiveRecord::Schema.define(version: 20150720093320) do
 
   create_table "attachments", force: :cascade do |t|
     t.string   "file"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
   end
+
+  add_index "attachments", ["question_id"], name: "index_attachments_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
