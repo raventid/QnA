a80b63fb7e22180f666a6c3a912f208241d48467
raventid <juliankul@gmail.com>
2015-07-20 11:07:00 +0300

    add first version of adding files to question spec

---
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -0,0 +1,27 @@
+require_relative 'acceptance_helper'
+
+feature 'Add files', %q{
+  In order to illustrate my question
+  As a question's author
+  I would like to be able to attach file to my question
+} do
+
+  given(:user) { create(:user) }
+
+  background do
+    sign_in(user)
+    visit new_question_path
+  end
+
+  scenario 'User adds file when asks question'do
+    fill_in 'Title', 'New question title'
+    fill_in 'Body', 'New question body'
+    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+    click_on 'Create'
+
+
+    expect(page).to have_content 'spec_helper.rb'
+
+  end
+end
