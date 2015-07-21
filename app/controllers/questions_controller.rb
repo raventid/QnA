be9b4d30d2496294ff7ce9eecf8c88e3fc29e50b
raventid <juliankul@gmail.com>
2015-07-21 08:15:32 +0300

    complete add question user story

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,7 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+    @question.attachments.build
   end
 
   def edit
@@ -58,6 +59,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, attachments_attributes: [:file])
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -38,6 +38,10 @@ RSpec.describe QuestionsController, type: :controller do
         expect(assigns(:question)).to be_a_new(Question)
       end
 
+      it 'builds new attachment for question' do
+         expect(assigns(:question).attachments.first).to be_a_new(Attachment)
+      end
+
       it { should render_template :new }
     end
 
