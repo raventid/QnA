fbc915da7c994c149489b1c357c53de7f2beaab9
raventid <juliankul@gmail.com>
2015-07-24 17:03:58 +0300

    update routes and answers partial

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   post 'answers/:id/best' => 'answers#best', as: :best
 
-  delete 'attachment/:id' => 'attachments#destroy', as: :remove_attachment 
+  resources :attachments, only: [:destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
